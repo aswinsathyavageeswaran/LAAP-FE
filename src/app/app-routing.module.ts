f783e86a3cde39081cd518400a11b33e079d0492
Mahesh_VN <mahesh.v@finastra.com>
2022-04-08 16:24:36 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   declarations: []
